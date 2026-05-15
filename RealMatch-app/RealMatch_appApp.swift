dd0e5c304cea46b1402ed4fe19c4afda69e80b1b
ns-develops <test.tt9061030@gmail.com>
2026-05-15 16:07:58 +0200

    Initial Commit

---
+++ RealMatch-app/RealMatch_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealMatch_appApp.swift
+//  RealMatch-app
+//
+//  Created by Natali Samaan on 2026-05-15.
+//
+
+import SwiftUI
+
+@main
+struct RealMatch_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
